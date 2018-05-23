bca63d8272f54388e3e01ff0f03ff57b06df3f69
Gergely Papi <gergely.papi.cc@gmail.com>
2018-05-23 13:06:41 +0200

    Added Appbar with login/register buttons

---
+++ src/Components/MiniUserAppBar.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import Button from "@material-ui/core/es/Button/Button";
+import {MiniUserConsumer} from "../Storage/MiniUserProvider";
+import RegistrationDialog from "./RegistrationDialog"
+import Avatar from "@material-ui/core/es/Avatar/Avatar";
+
+const styles = {
+    root: {
+        flexGrow: 1,
+    },
+    flex: {
+        flex: 1,
+    },
+    menuButton: {
+        marginLeft: -12,
+        marginRight: 20,
+    },
+};
+
+const appBarContent = (
+    <MiniUserConsumer>
+        {(value) => {
+            const { userName, isLoggedIn, logIn, logOut } = value;
+
+            return isLoggedIn ? (
+                <React.Fragment>
+                    {"Welcome " + userName}
+                    <RegistrationDialog/>
+                    <Button variant="raised" onClick={logOut}>Logout</Button>
+                </React.Fragment>
+            ) : (
+                <React.Fragment>
+                    <Button onClick={logIn}>Login</Button>
+                </React.Fragment>
+            )
+        }}
+    </MiniUserConsumer>
+);
+
+
+function MiniUserAppBar(props) {
+    const { classes } = props;
+    return (
+        <div className={classes.root}>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="title" color="inherit" className={classes.flex}>
+                        Mini User Application
+                    </Typography>
+                    {appBarContent}
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
+}
+
+MiniUserAppBar.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(MiniUserAppBar);
---
+++ src/Components/RegistrationDialog.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+class RegistrationDialog extends React.Component {
+    state = {
+        open: false,
+    };
+
+    handleClickOpen = () => {
+        this.setState({ open: true });
+    };
+
+    handleClose = () => {
+        this.setState({ open: false });
+    };
+
+    render() {
+        return (
+            <div>
+                <Button onClick={this.handleClickOpen}>Register User</Button>
+                <Dialog
+                    open={this.state.open}
+                    onClose={this.handleClose}
+                    aria-labelledby="alert-dialog-title"
+                    aria-describedby="alert-dialog-description"
+                >
+                    <DialogTitle id="alert-dialog-title">{"Use Google's location service?"}</DialogTitle>
+                    <DialogContent>
+                        <DialogContentText id="alert-dialog-description">
+                            Random Dialog Text
+                        </DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={this.handleClose} color="primary">
+                            Register
+                        </Button>
+                        <Button onClick={this.handleClose} color="primary" autoFocus>
+                            Cancel
+                        </Button>
+                    </DialogActions>
+                </Dialog>
+            </div>
+        );
+    }
+}
+
+export default RegistrationDialog;
